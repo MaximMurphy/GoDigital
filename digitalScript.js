59825b866625751606e4f6ee29622f9dd796a6cf
Maxim Murphy <maxim.murphy8@gmail.com>
2023-04-25 23:47:36 -0400

    update digital script

--- digitalScript.js
+++ digitalScript.js
@@ -17,23 +17,21 @@ navigator.mediaDevices
   .getUserMedia({ video: true, audio: false })
   .then((stream) => {
     s0.initCam();
-
-    src(s0)
-      .modulateScale(osc(8).rotate(Math.sin(time)), 0.5)
-      .thresh(0.3)
-
-      .modulateScale(osc(2).modulateRotate(o0, 0.74))
-      .diff(
-        src(o0)
-          .rotate([-0.012, 0.01, -0.002, 0])
-          .scrollY(0, [-1 / 199800, 0].fast(0.7))
-      )
-      .brightness([-0.17, -0.12].smooth().fast(2))
-      .out();
   })
   .catch((error) => {
     // Handle the error
     console.error(err);
   });
 
-s0.initCam();
+src(s0)
+  .modulateScale(osc(8).rotate(Math.sin(time)), 0.5)
+  .thresh(0.3)
+
+  .modulateScale(osc(2).modulateRotate(o0, 0.74))
+  .diff(
+    src(o0)
+      .rotate([-0.012, 0.01, -0.002, 0])
+      .scrollY(0, [-1 / 199800, 0].fast(0.7))
+  )
+  .brightness([-0.17, -0.12].smooth().fast(2))
+  .out();
